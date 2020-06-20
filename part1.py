83f3639db1df29222aa5ee3031a856744ce787c4
鍾昀諠 <luk1684tw@gmail.com>
2020-06-20 23:02:25 +0800

    add part1 and report

--- part1.py
+++ part1.py
@@ -2,21 +2,75 @@ import os
 from datetime import datetime as dt
 
 import numpy as np
+
 from PIL import Image
 from matplotlib import pyplot as plt
+from scipy.spatial.distance import cdist
+
 
 train_path = os.path.join('Yale_Face_Database', 'Training')
 test_path = os.path.join('Yale_Face_Database', 'Testing')
 
 
-def fetch_eigvecs(eig_val, eig_vec, k):
+def linear_kernel(x, y):
+    k = list()
+    for img in x:
+        k.append(np.dot(img, y.T))
+    k = np.array(k)
+
+    return k
+
+
+def rbf_kernel(x, y):
+    k = cdist(x, y, metric='sqeuclidean')
+    k = np.exp(-gamma*k)
+
+    return k
+
+
+def fetch_eigvecs(eig_val, eig_vec, k, kernel=None):
     eig_dict = dict(zip(eig_val, eig_vec.T))
     eig_val[::-1].sort()
     principle_component = eig_dict[eig_val[0]]
     for i in range(1, k):
-        principle_component = np.vstack((principle_component, eig_dict[eig_val[i]]))
+        if kernel:
+            principle_component = np.vstack((principle_component, eig_dict[eig_val[i]] / eig_val[i]))
+        else:
+            principle_component = np.vstack((principle_component, eig_dict[eig_val[i]]))
 
     return principle_component.T
+
+
+def KPCA(X, test, res_dim, kernel, n_neighbors):
+    # X row-based
+    k = kernel_funcs[kernel](X, X)
+    print ('kerenl shape:', k.shape)
+    
+    N = len(X)
+    ones = np.ones((N, N)) / N
+    k = k - np.dot(ones, k) - np.dot(k, ones) + np.dot(ones, np.dot(k, ones))
+
+    eig_val, eig_vec = np.linalg.eig(k)
+    alpha = fetch_eigvecs(eig_val, eig_vec, res_dim, kernel=1) # 135X25
+    print (alpha.shape)
+    train_img_lowD = np.dot(k, alpha)
+
+    prd_result = list()
+    for img in test:
+        img = img.reshape(1, -1)
+        
+        img_lowD = kernel_funcs[kernel](X, img).flatten()
+        img_lowD = np.sum((alpha.T*img_lowD).T, axis=0)
+        
+        distance = list()
+        for img in train_img_lowD:
+            distance.append(np.linalg.norm(img - img_lowD))
+        distance = np.array(distance)
+        # print (distance)
+        idx = np.argpartition(distance, n_neighbors)[:n_neighbors] // 9 + 1
+        prd_result.append(np.argmax(np.bincount(idx)))
+
+    return np.array(prd_result)
 
 
 def PCA(X, res_dim):
@@ -58,6 +112,42 @@ def predict_PCA(train_data, test_data, avg_face, train_W, n_neighbors):
         prd_result.append(np.argmax(np.bincount(idx)))
 
     return np.array(prd_result)
+
+
+def KLDA(train_imgs, k, kernel):
+    print (train_imgs.shape) # should be 135 X 10000
+    start = dt.now()
+    center_set = list()
+    within_scatter, between_scatter = 0, 0
+    print ('Start calculate SW...')
+    for i in range(15):
+        # calculate Sk and sum up all Sks
+        print (i)
+        data_i = train_imgs[9 * i : 9 * (i + 1), :]
+        center = np.mean(data_i, axis=0)
+        center_set.append(center)
+
+        for data in data_i:
+            diff = (data - center).reshape((-1, 1)).astype('float32')
+            within_scatter += kernel_funcs[kernel](diff, diff)
+
+    print (np.linalg.det(within_scatter))
+    center_set = np.array(center_set)
+    print (center_set.shape)
+    print ('Start calculate SB...')
+    center = np.mean(train_imgs, axis=0)
+    for center_i in center_set:
+        diff = (center_i - center).reshape((-1, 1)).astype('float32')
+        between_scatter += 9 * kernel_funcs[kernel](diff, diff)
+
+    print ((dt.now() - start).total_seconds())
+    print ('Start calculate W...')
+    eig_val, eig_vec = np.linalg.eig((np.linalg.pinv(within_scatter) * between_scatter))
+    W = fetch_eigvecs(eig_val, eig_vec.real, k)
+    # plot_face(5, 5, W.T, 'KLDA_FisherFace.png', (100, 100))
+
+    return W
+
 
 
 def LDA(train_imgs, k):
@@ -141,22 +231,25 @@ def read_images(path, scale=(195, 231)):
 
 
 if __name__ == "__main__":
-    # task1 & task2 of PCA
+    # Global hyper-parameters
     k = 25
+    reconst = np.random.randint(135, size=10)
+    gamma = 5e-9 # used in rbf kernel
+    kernel_funcs = {'linear': linear_kernel, 'rbf': rbf_kernel}
+
+    # task1 & task2 of PCA
     train_imgs = read_images(train_path, (195, 231)) # note PIL scale order
     print (train_imgs.shape)
     train_W, avg = PCA(train_imgs, k)
-    print (avg.shape)
     print ('eigen face shape:' , train_W.shape)
     print ('PCA done')
     plot_face(5, 5, train_W.T, 'PCA_EigenFace.png', (231, 195))
 
-    # print ('Reconstructing random 10 images')
-    reconst = np.random.randint(len(train_imgs), size=10)
-    # print (reconst)
-    plot_face(5, 2, train_imgs[reconst], 'PCA_Origin.png', (231, 195))
-    # res_mean = np.mean(train_imgs[reconst], axis=0)
-    # plot_face(5, 2, np.dot(train_imgs[reconst] - avg, np.dot(train_W, train_W.T)) + avg, 'PCA_Resconstruct.png', (231, 195))
+    print ('Reconstructing random 10 images')
+    print (reconst)
+    plot_face(5, 2, train_imgs[reconst], 'OriginFace.png', (231, 195))
+    res_mean = np.mean(train_imgs[reconst], axis=0)
+    plot_face(5, 2, np.dot(train_imgs[reconst] - avg, np.dot(train_W, train_W.T)) + avg, 'PCA_Resconstruct.png', (231, 195))
 
     print ('Predicting test images')
     test_imgs = read_images(test_path)
@@ -164,6 +257,13 @@ if __name__ == "__main__":
 
     prd_result = predict_PCA(train_imgs, test_imgs, avg, train_W, 3)
     print ('Accuracy of PCA: ', len(prd_result[prd_result == test_labels]) / 30)
+
+    # kernel PCA
+    prd_linear = KPCA(train_imgs, test_imgs, k, 'linear', 3)
+    prd_rbf = KPCA(train_imgs, test_imgs, k, 'rbf', 3)
+    print (prd_linear)
+    print ('Accuracy of KPCA_rbf: ', len(prd_rbf[prd_rbf == test_labels]) / 30)
+    print ('Accuracy of KPCA_linear: ', len(prd_linear[prd_linear == test_labels]) / 30)
 
     # task1 & task2 of LDA
     print ('Start LDA')
@@ -174,10 +274,21 @@ if __name__ == "__main__":
     print ((dt.now() - start).total_seconds())
 
     print ('Reconstructing random 10 images')
-
+    plot_face(5, 2, np.dot(np.dot(train_imgs[reconst], W), W.T), 'LDA_Reconstruct.png', (100, 100))
 
 
     print ('Predicting test images')
     prd_result = predict_LDA(train_imgs,test_imgs, W, 3)
     print (prd_result)
     print ('Accuracy of LDA: ', len(prd_result[prd_result == test_labels]) / 30)
+
+    # kernel FLD
+    W = KLDA(train_imgs, k, 'rbf')
+    prd_result = predict_LDA(train_imgs,test_imgs, W, 3)
+    print (prd_result)
+    print ('Accuracy of RBF KLDA: ', len(prd_result[prd_result == test_labels]) / 30)
+
+    W = KLDA(train_imgs, k, 'linear')
+    prd_result = predict_LDA(train_imgs,test_imgs, W, 3)
+    print (prd_result)
+    print ('Accuracy of Linear KLDA: ', len(prd_result[prd_result == test_labels]) / 30)
